c92e74412011146ee87244e7ef1f7e7a521ab9e1
arjunsoota <arjunsoota97@gmail.com>
2019-06-04 16:47:54 +0530

    included a model too to interact with the form

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from main import views
 
 urlpatterns= [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('students',views.students),
+    path('addstudents',views.addstudent)
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from main import forms
+from django.http import HttpResponseRedirect
+from main import (
+    forms,
+    models
+)
 # Create your views here.
 
 def index(request):
@@ -7,3 +11,22 @@ def index(request):
         'form' : forms.ExampleForm
     }
     return render(request,'index.html',context)
+
+def students(request):
+    students = models.Student.objects.all()
+    context ={
+        "students" : students
+    }
+    return render(request,'student.html',context)
+
+def addstudent(request):
+    studentform = forms.StudentForm()
+    if request.method =='POST':
+        studentform = forms.StudentForm(request.POST)
+        if studentform.is_valid():
+            student= studentform.save()
+            return HttpResponseRedirect('/students')
+    context ={
+        'studentform':studentform
+    }
+    return render(request,'add_student.html',context)
