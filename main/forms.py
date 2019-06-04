73b417f032d94e4c4e305502b49d061d92a50b77
arjunsoota <arjunsoota97@gmail.com>
2019-06-04 15:14:15 +0530

    first commit

---
+++ main/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ExampleForm(forms.Form):
+    name=forms.CharField(max_length=256)
+    about_me=forms.CharField(widget = forms.Textarea)
+    active = forms.BooleanField()
+
